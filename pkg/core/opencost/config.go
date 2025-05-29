0a3e677a8674322d2ca40c6da996ef1bdc9a0d04
Sean Holcomb <sean.holcomb@ibm.com>
2025-05-29 16:20:08 -0700

    Rename env variable and relocate storage initialization
    Signed-off-by: Sean Holcomb <sean.holcomb@ibm.com>

--- pkg/core/opencost/config.go
+++ pkg/core/opencost/config.go
@@ -3,16 +3,18 @@ package opencost
 import "github.com/opencost/opencost/pkg/env"
 
 type OpenCostConfig struct {
-	ConfigPath          string // env.GetConfigPathWithDefault("/var/configs/")
-	CloudProviderAPIKey string // env.GetCloudProviderAPIKey()
-	Promless            bool   // env.GetPromless()
+	ConfigPath                 string // env.GetConfigPathWithDefault("/var/configs/")
+	CloudProviderAPIKey        string // env.GetCloudProviderAPIKey()
+	CollectorDataSourceEnabled bool   // env.IsCollectorDataSourceEnabled()
+	BucketConfigFile           string // env.GetExportBucketConfigFile()
 }
 
 // NewOpenCostConfig creates a new OpenCostConfig with values parsed from the environment variables.
 func NewOpenCostConfigFromEnv() *OpenCostConfig {
 	return &OpenCostConfig{
-		ConfigPath:          env.GetConfigPathWithDefault("/var/configs/"),
-		CloudProviderAPIKey: env.GetCloudProviderAPIKey(),
-		Promless:            env.GetPromless(),
+		ConfigPath:                 env.GetConfigPathWithDefault("/var/configs/"),
+		CloudProviderAPIKey:        env.GetCloudProviderAPIKey(),
+		CollectorDataSourceEnabled: env.IsCollectorDataSourceEnabled(),
+		BucketConfigFile:           env.GetExportBucketConfigFile(),
 	}
 }
