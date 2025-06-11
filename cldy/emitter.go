3cc42fe8f61c86100ea79d1fda84a05ee6753777
alexsouthard <Alex.Southard@ibm.com>
2025-06-10 17:55:23 -0700

    Change to directory of filepath removal

--- cldy/emitter.go
+++ cldy/emitter.go
@@ -268,7 +268,7 @@ func (ce *Emitter) ClearAndRecreateScratchDir() error {
 			if err != nil {
 				log.Warnf("problem deleting file: %s", err)
 			}
-			ce.Uploader.RemoveSample(filePath)
+			ce.Uploader.RemoveSample(filepath.Dir(ce.ScratchPath))
 		}
 	}
 
