050cc29fce012968e7a0322b4bc642b68c134dc6
alexsouthard <Alex.Southard@ibm.com>
2025-06-25 16:39:42 -0700

    Add in missing return (duh)

--- cldy/emitter.go
+++ cldy/emitter.go
@@ -496,9 +496,9 @@ func (ce *Emitter) fetchAgentVersion(pods []*v1.Pod) error {
 	for _, pod := range pods {
 		for _, container := range pod.Spec.Containers {
 			if rContainer.MatchString(container.Name) {
-				log.Infof("Images: %s", container.Image)
 				if rTag.MatchString(container.Image) {
 					ce.agentVersion = rTag.FindString(container.Image)[1:]
+					return nil
 				}
 			}
 		}
