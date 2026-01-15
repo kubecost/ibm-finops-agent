1b72bafb671d7e9940ef5350e54957560a7b0196
alexsouthard <Alex.Southard@ibm.com>
2026-01-15 11:55:20 -0800

    Swap to gov2

--- cldy/clients.go
+++ cldy/clients.go
@@ -460,7 +460,7 @@ func getURLsFromRegion(region string) (string, string) {
 		return formatFrontdoorAndCloudabilityURLs("-in", "-in")
 	case "gov", "us-gov-west-1":
 		return formatFrontdoorAndCloudabilityURLs("-usgov", ".usgov")
-	case "gov-dod", "us-gov-east-1":
+	case "gov2", "us-gov-east-1":
 		return formatFrontdoorAndCloudabilityURLs("-usgov2", ".usgov2")
 	case "hybrid-eu":
 		return formatFrontdoorAndCloudabilityURLs("-eu", "")
