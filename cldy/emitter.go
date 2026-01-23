1346a19d8d987c3c735c87850e901ea8f8ffbcdd
alexsouthard <Alex.Southard@ibm.com>
2026-01-23 12:12:21 -0800

    Add trim on reading secrets from volume

--- cldy/emitter.go
+++ cldy/emitter.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -172,7 +173,8 @@ func getSecretFromFileVolume(filepath string) string {
 		log.Warnf("error attempting to collect secret from file: %s with err: %v", filepath, err)
 		return ""
 	}
-	return string(key)
+	// Trim space on secrets
+	return strings.TrimSpace(string(key))
 }
 
 func (ce *Emitter) ID() emitter.EmitterID {
